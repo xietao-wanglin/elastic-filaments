17c0f2ee6412cdf110f16c1510c766307f1d9259
Xietao Wang Lin <Xietao.Wang-Lin@warwick.ac.uk>
2024-08-15 11:52:51 +0100

    change initial weights

--- pinn.py
+++ pinn.py
@@ -92,7 +92,7 @@ if __name__ == "__main__":
         num_test=10000,
     )
 
-    net = dde.nn.FNN([3] + [20] * 4 + [1], "tanh", "Glorot normal")
+    net = dde.nn.FNN([3] + [20] * 4 + [1], "tanh", "Glorot uniform")
     net.apply_output_transform(output_transform)
     model = dde.Model(data, net)
 
