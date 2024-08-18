a88c58405197866019a710c8e227f40345381d30
Xietao Wang Lin <Xietao.Wang-Lin@warwick.ac.uk>
2024-08-18 23:47:43 +0100

    snippets

--- launcher.py
+++ launcher.py
@@ -5,10 +5,11 @@ from simulation import Simulation
 
 if __name__ == "__main__":
 
+    eta = 40
     sim = Simulation(timesteps=1000,
-                     n_points=50,
+                     n_points=200,
                      max_time=1, osc_freq=1,
-                     length=1, eta=1,)
+                     length=1.5, eta=eta)
     
     sim.run(verbose=True)
-    sim.create_video()
+    sim.save_data(filename=f'filament_1.5_{eta}')
